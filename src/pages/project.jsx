fcdbfb14bbf926517f362ade8f8f6f4485353183
Mohamed-Afran <iam.mohamed.afran@gmail.com>
2025-07-10 12:52:46 +0530

    changed on 10/07/25 12:59pm

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -19,54 +19,78 @@ export default function Projects(){
     return(
         <>
      
-      <div className="container text-center mt-5 pt-5 mb-5" id="project">
-  <h1>projects</h1>
+      <div  id="project">
+  <h1 className="text-center ">projects</h1>
 
   <div className={`${proj.projects}`} >
     <div   data-aos="fade" >
-      <Paper className="h-100" elevation={5} sx={paperStyle}>
+     <Paper
+  className="h-100 d-flex flex-column"
+  elevation={5}
+  sx={paperStyle}
+>
         <img src={pic} alt="pix" />
         <h1>Nexus event</h1>
         <p> this project developed for the college level hackathon competion and this project is mainly focused on the announcements of the events in the colleges through their official website. 
         this project enables to apply for the respective event, or any placement related meetings are to be conducted by the college</p>
-        <div className={proj.stack}>
+  <div className={`${proj.stack} mt-auto`}>
           <i className="devicon-html5-plain-wordmark colored"></i>
           <i className="devicon-css3-plain-wordmark colored"></i>
           <i className="devicon-javascript-plain colored"></i>
           <i className="devicon-mysql-plain-wordmark"></i>
           <i className="devicon-php-plain colored"></i>
         </div>
-        <button type="text" className={proj.glowBtn}>git hub</button>
+        <div className="mt-auto">
+    <button type="text" className={`${proj.glowBtn}`}>
+      git hub
+    </button>
+  </div>
       </Paper>
     </div> 
  
     <div   data-aos="fade"  >
-      <Paper className="h-100" elevation={5} sx={paperStyle}>
+     <Paper
+  className="h-100 d-flex flex-column"
+  elevation={5}
+  sx={paperStyle}
+>
         <img src={pic} alt="pix" />
         <h1>Nexus event</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus adipisci eos itaque cupiditate odit nulla maxime asperiores, tenetur velit praesentium, cumque quasi illo dolores at quibusdam? Omnis nihil dicta facere.</p>
-        <div className={proj.stack}>
+      <div className={`${proj.stack} mt-auto`}>
           <i className="devicon-html5-plain-wordmark colored"></i>
           <i className="devicon-css3-plain-wordmark colored"></i>
           <i className="devicon-javascript-plain colored"></i>
           <i className="devicon-react-original-wordmark colored"></i>
         </div>
-        <button type="text" className={proj.glowBtn}>git hub</button>
+        <div className="mt-auto">
+    <button type="text" className={`${proj.glowBtn}`}>
+      git hub
+    </button>
+  </div>
       </Paper>
     </div> 
  
     <div   data-aos="fade" >
-      <Paper className="h-100" elevation={5} sx={paperStyle}>
+     <Paper
+  className="h-100 d-flex flex-column"
+  elevation={5}
+  sx={paperStyle}
+>
         <img src={pic} alt="pix" />
         <h1>Nexus event</h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel obcaecati, accusantium minus alias exercitationem ratione reiciendis quod molestiae sit iste temporibus quaerat hic aspernatur minima non repellendus magni tenetur esse?</p>
-        <div className={`${proj.stack} "text-center"`}>
+        <div className={`${proj.stack} mt-auto`}>
           <i className="devicon-html5-plain-wordmark colored"></i>
           <i className="devicon-css3-plain-wordmark colored"></i>
           <i className="devicon-javascript-plain colored"></i>
           <i className="devicon-react-original-wordmark colored"></i>
         </div>
-        <button type="text" className={proj.glowBtn}>git hub</button>
+        <div className="mt-auto">
+    <button type="text" className={`${proj.glowBtn}`}>
+      git hub
+    </button>
+  </div>
       </Paper>
     </div>
   </div>
